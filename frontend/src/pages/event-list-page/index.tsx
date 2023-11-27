bb7ab8e17497bae57044c07b4b7aa9e14bd95987
JoaoVictorCX <joaovictorcoelhox@gmail.com>
2023-11-27 17:37:31 -0300

    added event type and some mocked events

--- frontend/src/pages/event-list-page/index.tsx
+++ frontend/src/pages/event-list-page/index.tsx
@@ -3,11 +3,61 @@ import Header from "./components/header"
 import NewEventButton from "./components/new-event-button"
 import styled from "styled-components"
 
-export type Event = {
+
+export type Athletes = {
     name: string,
-    date: string
+    gender: 'Male' | 'Female',
+    shirt: 'P' | 'M' | 'G' | 'GG'
+} 
+
+export type Event = {
+    id: string,
+    thumbnail: string,
+    name: string,
+    status: 'Em espera' | 'Em andamento' | 'Concluído',
+    date: Date,
+    categories: string[],
+    scoreType: 'Por posição',
+    athletes: Athletes[],
+    workouts: object[],
+    results: object[]
 }
-const EVENTS: Event[] =[{name: 'MURPH', date: '09/12/2023'}, {name: 'HAPPY HOUR', date: '10/12/2023'}]
+const EVENTS: Event[] =[{
+    id: '1',
+    thumbnail: 'murph.png',
+    name: 'MURPH',
+    status: 'Em andamento',
+    date: new Date(),
+    categories: ['RX'],
+    scoreType: 'Por posição',
+    athletes: [{name: 'João Victor Coelho', gender: 'Male', shirt: 'GG'}],
+    workouts: [],
+    results: []
+},
+{
+    id: '2',
+    thumbnail: 'summer.png',
+    name: 'SUMMER CHALLENGE',
+    status: 'Em espera',
+    date: new Date(),
+    categories: ['RX'],
+    scoreType: 'Por posição',
+    athletes: [{name: 'João Victor Coelho', gender: 'Male', shirt: 'GG'}],
+    workouts: [],
+    results: []
+},
+{
+    id: '3',
+    name: 'BXTRIATHLON',
+    thumbnail: 'challenge.png',
+    status: 'Concluído',
+    date: new Date(),
+    categories: ['RX'],
+    scoreType: 'Por posição',
+    athletes: [{name: 'João Victor Coelho', gender: 'Male', shirt: 'GG'}],
+    workouts: [],
+    results: []
+}]
 
 function EventListPage() {
     const gerarComponents = (e: Event) => <EventCard key={e.name} event={e} />
